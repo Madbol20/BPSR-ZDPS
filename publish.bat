4081ec699aae3837a3e0f636e1a18f2658105306
seilent <github@seilent.net>
2026-01-01 10:23:15 +0900

    small changes in publish

--- publish.bat
+++ publish.bat
@@ -1,3 +1,2 @@
-dotnet publish "BPSR-ZDPS/BPSR-ZDPS.csproj" -r win-x64 -c Release -o ./publish /p:PublishSingleFile=true /p:PublishTrimmed=false /p:TrimMode=Link /p:IncludeAllContentForSelfExtract=false --self-contained false
-move "publish\BPSR-ZDPS.exe" "publish\BPSR-ZDPS.exe"
-copy "BPSR-ZDPS\Data" "publish\Data"
+dotnet publish "BPSR-ZDPS/BPSR-ZDPS.csproj" -r win-x64 -c Release -o ./publish -p:PublishSingleFile=true -p:PublishTrimmed=false -p:TrimMode=link -p:IncludeAllContentForSelfExtract=false -p:DebugType=None -p:DebugSymbols=false --self-contained false
+if exist "publish\*.pdb" del /q "publish\*.pdb"
